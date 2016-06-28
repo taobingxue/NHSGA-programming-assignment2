a2e3cd92134d66d8e4142c5fa7e0da6be80c9f44
taobingxue <taobingxue001@126.com>
2016-06-28 16:33:05 -0400

    put them here :P

---
+++ Assets/Scripts/brick_generator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brick_generator : MonoBehaviour {
+	// These variables are just reminder and for your convenient, delete them if you want to.
+	public GameObject brick_prefab;
+	public int N, M;
+	public float gap_x, gap_z, start_z;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		 * To Do:
+		 * 	 Generate bricks here. Bricks means more than one, and they do not overlap with each other.
+		 *   
+		 *   You can generate what I demoed in the class, the N*M bricks with gap_x and gap_z starts from start_z.
+		 * 
+		 *   But if you want to have fun with yourself, create whatever you want!
+		 * 
+		 */
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		 * If you want to try some cool real-time generate mechanic, you can have a lot fun here.
+		 * 
+		 */
+
+	}
+}
---
+++ Assets/Scripts/platform_controler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platform_controler : MonoBehaviour {
+	// These variables are just reminder and for your convenient, delete them if you want to.
+	public int platform_speed;
+	public float limit_l, limit_r;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
